99fff0fc1c7873f0e3c31ab124e623a8f91d3b62
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-10 15:05:49 +0700

    Stabilize SmartEyeX build with GitHub Gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 plugins {
- kotlin("jvm") version "1.9.10" apply false
- id("com.android.application") version "8.1.0" apply false
-}
-dependencyResolutionManagement {
- repositoriesMode.set(org.gradle.api.initialization.dsl.RepositoriesMode.FAIL_ON_PROJECT_REPOS)
- repositories {
- google()
- mavenCentral()
- }
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
 }
