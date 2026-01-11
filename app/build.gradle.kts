d04414f9db097cd9d74efca68ceaea31201fc869
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-11 21:23:20 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,9 +67,9 @@ dependencies {
     implementation("androidx.camera:camera-view:1.3.2")
 
     // Lifecycle + Coroutines
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
+implementation("androidx.activity:activity-ktx:1.9.0")
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
