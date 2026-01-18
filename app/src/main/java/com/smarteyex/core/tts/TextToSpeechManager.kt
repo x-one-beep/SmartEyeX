1d9735c1aae912df0d37cdf532fda5831c37d68b
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-18 23:17:28 +0700

    Update TextToSpeechManager.kt

--- app/src/main/java/com/smarteyex/core/tts/TextToSpeechManager.kt
+++ app/src/main/java/com/smarteyex/core/tts/TextToSpeechManager.kt
@@ -6,18 +6,22 @@ import java.util.*
 
 class TextToSpeechManager(context: Context) {
 
-    private val tts = TextToSpeech(context) { status ->
-        if (status == TextToSpeech.SUCCESS) {
-            tts.language = Locale("id", "ID")
+    private var tts: TextToSpeech? = null
+
+    init {
+        tts = TextToSpeech(context) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                tts?.language = Locale("id", "ID")
+            }
         }
     }
 
     fun speak(text: String) {
-        tts.speak(text, TextToSpeech.QUEUE_ADD, null, null)
+        tts?.speak(text, TextToSpeech.QUEUE_ADD, null, null)
     }
 
     fun shutdown() {
-        tts.stop()
-        tts.shutdown()
+        tts?.stop()
+        tts?.shutdown()
     }
 }
