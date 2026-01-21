f686a81d821bd05cf184fd8ce77af7fbc882e1f1
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-21 20:38:46 +0700

    Update SettingsActivity.kt

--- app/src/main/java/com/smarteyex/core/ui/SettingsActivity.kt
+++ app/src/main/java/com/smarteyex/core/ui/SettingsActivity.kt
@@ -1,4 +1,4 @@
-package com.smarteyex.core.ui
+package com.smarteyex.core
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
