5e284aa8f9b02ce7bd2d9941ed8b893c103c3d60
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-26 15:20:24 +0700

    Update VoiceEngine.kt

--- app/src/main/java/com/smarteyex/core/VoiceEngine.kt
+++ app/src/main/java/com/smarteyex/core/VoiceEngine.kt
@@ -6,26 +6,19 @@ import java.util.Locale
 
 class VoiceEngine(private val context: Context) {
 
-    private lateinit var tts: TextToSpeech
+    private var isReady = false
 
-    fun init() {
-        tts = TextToSpeech(context) { status ->
-            if (status == TextToSpeech.SUCCESS) {
-                tts.language = Locale("id", "ID")
-            }
+fun init() {
+    tts = TextToSpeech(context) { status ->
+        if (status == TextToSpeech.SUCCESS) {
+            tts.language = Locale("id", "ID")
+            isReady = true
         }
     }
+}
 
-    fun speak(text: String) {
-        if (::tts.isInitialized) tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "VOICE_ENGINE")
-    }
-
-    fun shutdown() {
-        if (::tts.isInitialized) tts.shutdown()
-    }
-
-    // Method untuk MainActivity, start listening
-    fun startListening() {
-        speak("Bung Smart siap mendengar")
+fun speak(text: String) {
+    if (::tts.isInitialized && isReady) {
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "VOICE_ENGINE")
     }
 }
