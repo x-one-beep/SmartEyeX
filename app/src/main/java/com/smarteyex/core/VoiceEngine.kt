c1828414b7e7b24fd87dd1bf241e92c9aacc267d
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-26 11:38:26 +0700

    Update VoiceEngine.kt

--- app/src/main/java/com/smarteyex/core/VoiceEngine.kt
+++ app/src/main/java/com/smarteyex/core/VoiceEngine.kt
@@ -1,42 +1,16 @@
 package com.smarteyex.core
 
 import android.content.Context
-import android.content.Intent
-import android.speech.RecognizerIntent
-import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
-import com.smarteyex.core.ai.GroqAiEngine
 import java.util.*
 
-class VoiceEngine(private val context: Context) {
+class VoiceEngine(context: Context) {
 
-    private val speechRecognizer =
-        SpeechRecognizer.createSpeechRecognizer(context)
-
-    private lateinit var tts: TextToSpeech
-    private lateinit var processor: SpeechCommandProcessor
-
-    fun init(ai: GroqAiEngine) {
-        tts = TextToSpeech(context) {
-            tts.language = Locale("id", "ID")
-        }
-        processor = SpeechCommandProcessor(context, tts, ai)
-        speechRecognizer.setRecognitionListener(processor)
+    private val tts = TextToSpeech(context) {
+        tts.language = Locale("id","ID")
     }
 
-    fun startListening() {
-        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
-            putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-            )
-            putExtra(RecognizerIntent.EXTRA_LANGUAGE, "id-ID")
-        }
-        speechRecognizer.startListening(intent)
-    }
-
-    fun stop() {
-        speechRecognizer.stopListening()
-        tts.shutdown()
+    fun speak(text: String) {
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "VOICE_ENGINE")
     }
 }
