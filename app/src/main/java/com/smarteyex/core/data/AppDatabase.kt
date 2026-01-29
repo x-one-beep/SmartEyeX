a81e85e29824b7921e52c2e67c5ea8e1b635ceb9
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-29 11:28:48 +0700

    Update AppDatabase.kt

--- app/src/main/java/com/smarteyex/core/data/AppDatabase.kt
+++ app/src/main/java/com/smarteyex/core/data/AppDatabase.kt
@@ -5,7 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Event::class], version = 1)
+@Database(
+    entities = [...],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun eventDao(): EventDao
 
