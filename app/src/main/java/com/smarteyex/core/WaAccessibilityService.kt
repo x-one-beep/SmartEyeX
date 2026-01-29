365aafdbb165a0e9e370e7fdf4d1798a88d89f70
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-29 21:28:08 +0700

    Create WaAccessibilityService.kt

---
+++ app/src/main/java/com/smarteyex/core/WaAccessibilityService.kt
@@ -0,0 +1,35 @@
+package com.smarteyex.core.wa
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class WaAccessibilityService: AccessibilityService() {
+
+    companion object{
+        var pendingText=""
+        var sendNow=false
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+
+        if(!sendNow) return
+
+        rootInActiveWindow?.findAccessibilityNodeInfosByText("Type a message")
+            ?.firstOrNull()?.apply{
+                performAction(AccessibilityNodeInfo.ACTION_SET_TEXT,
+                    android.os.Bundle().apply{
+                        putCharSequence(
+                          AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE,
+                          pendingText)
+                    })
+            }
+
+        rootInActiveWindow?.findAccessibilityNodeInfosByText("Send")
+            ?.firstOrNull()?.performAction(
+                AccessibilityNodeInfo.ACTION_CLICK)
+
+        sendNow=false
+    }
+
+    override fun onInterrupt() {}
+}
