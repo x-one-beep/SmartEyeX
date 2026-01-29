bd34f815c8b98930568cbbcc0ebc96b0b8fcda7e
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-29 22:23:01 +0700

    Update WaNotificationListener.kt

--- app/src/main/java/com/smarteyex/core/WaNotificationListener.kt
+++ app/src/main/java/com/smarteyex/core/WaNotificationListener.kt
@@ -13,13 +13,20 @@ class WaNotificationListener: NotificationListenerService() {
         if(sbn.packageName != "com.whatsapp") return
 
         val extras = sbn.notification.extras
-        val msg = extras.getCharSequence("android.text")?.toString() ?: return
-        val title = extras.getCharSequence("android.title")?.toString() ?: return
+        val msg = extras.getCharSequence("android.text")?.toString()
+    ?: extras.getCharSequence("android.bigText")?.toString()
+    ?: return
+
+val title = extras.getCharSequence("android.title")?.toString() ?: return
 
         val isGroup = title.contains(":")
         val sender = if(isGroup) title.substringAfter(":").trim() else title
         val group = if(isGroup) title.substringBefore(":") else null
-
+if(AppState.speaking){
+    WaReplyManager.queueSilent(sender,msg,isGroup,group)
+}else{
+    WaReplyManager.onIncoming(sender,msg,isGroup,group)
+}
         WaBus.onMessage(sender,msg,isGroup,group)
     }
 }
