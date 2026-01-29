9e6b458b28497d50bffa46bd3df6ac9073bf2b77
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-29 21:02:45 +0700

    Update WaNotificationListener.kt

--- app/src/main/java/com/smarteyex/core/WaNotificationListener.kt
+++ app/src/main/java/com/smarteyex/core/WaNotificationListener.kt
@@ -1,27 +1,31 @@
 package com.smarteyex.core.wa
 
-import android.app.Notification
-import android.content.Intent
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.content.Intent
 import com.smarteyex.core.VoiceService
 
 class WaNotificationListener : NotificationListenerService() {
 
+    private lateinit var waReply: WaReplyManager
+
+    override fun onCreate() {
+        super.onCreate()
+        waReply = WaReplyManager(this)
+    }
+
     override fun onNotificationPosted(sbn: StatusBarNotification) {
-        if (!sbn.packageName.contains("com.whatsapp")) return
+
+        if (sbn.packageName != "com.whatsapp") return
 
         val extras = sbn.notification.extras
-        val sender = extras.getString(Notification.EXTRA_TITLE) ?: return
-        val message = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString() ?: return
 
-        val intent = Intent(this, VoiceService::class.java).apply {
-            action = "WA_MESSAGE"
-            putExtra("sbn", sbn)
-            putExtra("sender", sender)
-            putExtra("message", message)
-        }
+        val sender =
+            extras.getCharSequence("android.title")?.toString() ?: return
 
-        startService(intent)
+        val message =
+            extras.getCharSequence("android.text")?.toString() ?: return
+
+        waReply.readIncoming(sender, message)
     }
 }
