602095bad24efad0a4debbed920017b1ec832171
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-29 21:23:23 +0700

    Update WaNotificationListener.kt

--- app/src/main/java/com/smarteyex/core/WaNotificationListener.kt
+++ app/src/main/java/com/smarteyex/core/WaNotificationListener.kt
@@ -2,30 +2,24 @@ package com.smarteyex.core.wa
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
-import android.content.Intent
-import com.smarteyex.core.VoiceService
 
-class WaNotificationListener : NotificationListenerService() {
+object WaBus {
+    var onMessage:(String,String,Boolean,String?)->Unit = {_,_,_,_->}
+}
 
-    private lateinit var waReply: WaReplyManager
-
-    override fun onCreate() {
-        super.onCreate()
-        waReply = WaReplyManager(this)
-    }
+class WaNotificationListener: NotificationListenerService() {
 
     override fun onNotificationPosted(sbn: StatusBarNotification) {
-
-        if (sbn.packageName != "com.whatsapp") return
+        if(sbn.packageName != "com.whatsapp") return
 
         val extras = sbn.notification.extras
+        val msg = extras.getCharSequence("android.text")?.toString() ?: return
+        val title = extras.getCharSequence("android.title")?.toString() ?: return
 
-        val sender =
-            extras.getCharSequence("android.title")?.toString() ?: return
+        val isGroup = title.contains(":")
+        val sender = if(isGroup) title.substringAfter(":").trim() else title
+        val group = if(isGroup) title.substringBefore(":") else null
 
-        val message =
-            extras.getCharSequence("android.text")?.toString() ?: return
-
-        waReply.readIncoming(sender, message)
+        WaBus.onMessage(sender,msg,isGroup,group)
     }
 }
