f6aaa3426dd72768716cbd15310da2626ef5a5bd
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-01-30 21:05:55 +0700

    Update DelayManager.kt

--- app/src/main/java/com/smarteyex/core/DelayManager.kt
+++ app/src/main/java/com/smarteyex/core/DelayManager.kt
@@ -3,6 +3,7 @@ package com.smarteyex.core.wa
 import android.os.Handler
 import android.os.Looper
 import com.smarteyex.core.VoiceService
+import com.smarteyex.core.AppSpeak
 
 object DelayManager {
 
@@ -10,7 +11,7 @@ object DelayManager {
         val minutes = Regex("\\d+").find(text)?.value?.toInt() ?: return
 
         Handler(Looper.getMainLooper()).postDelayed({
-            VoiceService.speakGlobal("Waktu tunggu $minutes menit selesai")
+            AppSpeak.speak("Waktu tunggu $minutes menit selesai")
         }, minutes*60*1000L)
     }
 }
