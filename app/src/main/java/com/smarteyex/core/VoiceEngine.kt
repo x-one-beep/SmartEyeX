242dda36a9d2e6b646c9e49f09b3c6ae055137a1
x-one-beep <fajar.muhaji36@smk.belajar.id>
2026-02-05 07:25:05 +0700

    Create VoiceEngine.kt

---
+++ app/src/main/java/com/smarteyex/core/VoiceEngine.kt
@@ -0,0 +1,35 @@
+package com.smarteyex.core.voice
+
+import android.content.Context
+import android.speech.SpeechRecognizer
+import android.speech.RecognizerIntent
+import com.smarteyex.core.AppState
+
+object VoiceEngine {
+
+    private lateinit var recognizer: SpeechRecognizer
+    private lateinit var context: Context
+
+    fun start(ctx: Context) {
+        context = ctx
+        recognizer = SpeechRecognizer.createSpeechRecognizer(ctx)
+
+        recognizer.setRecognitionListener(VoiceListener)
+        listen()
+    }
+
+    private fun listen() {
+        val intent = RecognizerIntent().apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
+        }
+        recognizer.startListening(intent)
+        AppState.isListening.set(true)
+    }
+
+    fun stop() {
+        recognizer.stopListening()
+        AppState.isListening.set(false)
+    }
+}
